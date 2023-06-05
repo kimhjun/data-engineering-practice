878548ccf7781035f10f0585367396a6de6cdbcd
kimhjun <hjun.kim@outlook.com>
2023-06-05 19:03:36 +0900

    revise query

--- 02. load_file_and_simple_analysis/make_dw_star_schema.sql
+++ 02. load_file_and_simple_analysis/make_dw_star_schema.sql
@@ -53,15 +53,13 @@ CREATE TABLE dimStore (
 	postal_code varchar(10),
 	manager_first_name varchar(45) NOT NULL,
 	manager_last_name varchar(45) NOT NULL,
-	phone varchar(20) NOT NULL,
-	create_date timestamp NOT NULL,
 	start_date date NOT NULL,
 	end_date date NOT NULL
 );
 
 INSERT INTO dimDate (date_key, date, year, quarter, month, day, week, is_weekend)
 SELECT DISTINCT
-	(to_char(payment_date::DATE, 'yyyMMDD')::integer) AS date_key,
+	(to_char(payment_date::DATE, 'yyyyMMDD')::integer) AS date_key,
 	date(payment_date) AS date,
 	extract(year FROM payment_date) AS year,
 	extract(quarter FROM payment_date) AS quarter,
@@ -102,10 +100,9 @@ JOIN country co ON (ci.country_id=co.country_id);
 
 
 INSERT INTO dimStore (store_key, store_id, address, address2, district, city, country, postal_code,
-						manager_first_name, manager_last_name, phone, create_date, start_date, end_date)
+						manager_first_name, manager_last_name, start_date, end_date)
 SELECT s.store_id as store_key,
 	   s.store_id,
-	   
 		a.address as address,
 	   a.address2 as address2,
 	   a.district as district,
@@ -114,7 +111,6 @@ SELECT s.store_id as store_key,
 	   a.postal_code as postal_code,
 	   st.first_name as manager_first_name,
 	   st.last_name as manager_last_name,
-	   a.phone as phone,
 	   now() as start_date,
 	   now() as end_date
 FROM store s
@@ -122,3 +118,41 @@ JOIN address a ON (s.address_id=a.address_id)
 JOIN city ci ON (a.city_id=ci.city_id)
 JOIN country co ON (ci.country_id=co.country_id)
 JOIN staff st ON (s.store_id=st.store_id);
+
+INSERT INTO dimMovie (movie_key, film_id, title, description, release_year, language, rental_duration, length, ratings, special_features)
+SELECT f.film_id as movie_key,
+	   f.film_id,
+	   f.title,
+	   f.description,
+	   f.release_year,
+	   l.name as language,
+	   f.rental_duration,
+	   f.length,
+	   f.rating,
+	   f.special_features
+FROM film f
+JOIN language l ON f.language_id = l.language_id
+;
+
+
+CREATE TABLE factSales
+(
+	sales_key SERIAL PRIMARY KEY,
+	date_key integer REFERENCES dimDate (date_key),
+	customer_key integer REFERENCES dimCustomer (customer_key),
+	movie_key integer REFERENCES dimMovie (movie_key),
+	store_key integer REFERENCES dimStore (store_key),
+	sales_amount numeric
+);
+
+
+INSERT INTO factSales (date_key, customer_key, movie_key, store_key, sales_amount)
+SELECT
+	TO_CHAR(payment_date :: DATE, 'yyyyMMDD')::integer AS date_key,
+	p.customer_id as customer_key,
+	i.film_id as movie_key,
+	i.store_id as store_key,
+	p.amount as sales_amount
+FROM payment p
+JOIN rental r ON p.rental_id = r.rental_id
+JOIN inventory i ON r.inventory_id = i.inventory_id;
